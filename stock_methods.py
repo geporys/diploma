dea724eef1d38cc0f3aacfa3a975e1f8360922eb
German Ignatovich <ger266.u@yandex.ru>
2021-05-29 11:15:23 +0300

    new improvments

--- stock_methods.py
+++ stock_methods.py
@@ -5,180 +5,171 @@ import datetime
 import matplotlib.pyplot as plt
 import calendar
 import time
+from datetime import datetime, date, timedelta
+from polygon import RESTClient
+import talib
 
 
 def get_data_by_hour(symbol):
-    url = f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?region=US&lang=en-US&includePrePost=false&interval=1h&useYfid=true&range=1y&corsDomain=finance.yahoo.com&.tsrc=finance"
-    yahooResponse = requests.get(url)
-    yahooData = yahooResponse.json()['chart']['result'][0]
-    data = {
-        'open': yahooData['indicators']['quote'][0]['open'],
-        'high': yahooData['indicators']['quote'][0]['high'],
-        'close': yahooData['indicators']['quote'][0]['close'],
-        'volume': yahooData['indicators']['quote'][0]['volume'],
-        'low': yahooData['indicators']['quote'][0]['low']
-    }
-    handledData = pd.DataFrame(data=data, index=pd.to_datetime(
-        np.array(yahooData['timestamp']) * 1000, unit='ms').to_pydatetime())
+    key = 'YZj_Ugd4fkCRX57STH5MeItRlAz31DRN'
+    client = RESTClient(key)
+    query = True
+    from_date = "2012-01-01"
+    to_date = (datetime.today().date() +
+               timedelta(days=1)).strftime("%Y-%m-%d")
+    now_date = datetime.today().date().strftime("%Y-%m-%d")
+    data_for_df = []
+    while query:
+        print(from_date)
+        resp = client.stocks_equities_aggregates(
+            symbol, 1, "hour", from_date, to_date, unadjusted=True, limit=50000)
+        if 'results' in dir(resp):
+            results = resp.results
+            data_for_df = data_for_df + results[:-1]
+            date = datetime.fromtimestamp((resp.results[-1]['t'])/1000.0)
+            from_date = date.date().strftime("%Y-%m-%d")
+            if from_date == now_date:
+                query = False
+        else:
+            query = False
+
+    handledData = pd.DataFrame(data=data_for_df)
+    handledData.columns = ['volume', 'vw', 'open',
+                           'close', 'high', 'low', 'time', 'number']
+    handledData.drop_duplicates(subset=['time'], inplace=True)
+    handledData.index = pd.to_datetime(
+        handledData['time'].values, unit='ms').to_pydatetime()
+    handledData.drop('time', axis=1, inplace=True)
     handledData.dropna(inplace=True)
+
     plt.figure(figsize=(15, 5))
     plt.plot(handledData.index, handledData['close'])
     plt.grid(True)
     return handledData
 
 
-def get_data_by_min(symbol):
-    period2 = calendar.timegm(time.gmtime())
-    objects = []
-    data = pd.DataFrame()
-    for i in range(4):
-        period1 = period2 - 604800
-        url = f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?symbol={symbol}&period1={period1}&period2={period2}&useYfid=true&interval=1m&includePrePost=true&events=div%7Csplit%7Cearn&lang=en-US&region=US&crumb=Uvl2JTAUYao&corsDomain=finance.yahoo.com"
-        period2 = period1
-        yahooResponse = requests.get(url)
-        yahooData = yahooResponse.json()['chart']['result'][0]
-        object_for_df = {
-            'open': yahooData['indicators']['quote'][0]['open'],
-            'high': yahooData['indicators']['quote'][0]['high'],
-            'close': yahooData['indicators']['quote'][0]['close'],
-            'volume': yahooData['indicators']['quote'][0]['volume'],
-            'low': yahooData['indicators']['quote'][0]['low']
-        }
-        objects.append(
-            {'data': object_for_df, 'timestamp': np.array(yahooData['timestamp'])})
-    for i in range(-3, 1):
-        handledData = pd.DataFrame(data=objects[-i]['data'], index=pd.to_datetime(
-            objects[-i]['timestamp'] * 1000, unit='ms').to_pydatetime())
-        handledData[handledData['volume'] == 0] = None
-        handledData.dropna(inplace=True)
-        data.append(handledData)
-    data.dropna(inplace=True)
-    plt.figure(figsize=(15, 5))
-    plt.plot(handledData.index, handledData['close'])
-    plt.grid(True)
-    return handledData
+def get_Daily_Volatility(close, span0=24):
+    # simple percentage returns
+    df0 = close.pct_change()
+    # 20 days, a month EWM's std as boundary
+    df0 = df0.ewm(span=span0).std()
+    df0.dropna(inplace=True)
+    return df0
 
 
-class Stock:
-    def cusum(self, raw, h, data):
-        tEvents, values, sPos, sNeg = [], [], 0, 0
-        diff = raw.diff()
-        for i in diff.index[1:]:
-            sPos, sNeg = max(0, sPos+diff.loc[i]), min(0, sNeg+diff.loc[i])
-            if sNeg < -h:
-                sNeg = 0
-                tEvents.append(i)
-                values.append(raw[i])
-            elif sPos > h:
-                sPos = 0
-                tEvents.append(i)
-                values.append(raw[i])
-        events = pd.DataFrame(data=values, index=tEvents)
-        plt.figure(figsize=(15, 5))
-        plt.plot(data.index, raw)
-        plt.plot(tEvents, values, '.')
-        plt.title('cusum')
-        plt.grid(True)
-
-        plt.figure(figsize=(15, 5))
-        plt.plot(data.index[-100:], raw[-100:])
-        plt.plot(tEvents[-20:], values[-20:], '.')
-        plt.title('cusum last 100')
-        plt.grid(True)
-        return events
-
-    def get_horizons(self, prices, delta):
-        t1 = prices.index.searchsorted(prices.index + delta)
-        t1 = t1[t1 < prices.shape[0]]
-        t1 = prices.index[t1]
-        t1 = pd.Series(t1, index=prices.index[:t1.shape[0]])
-        return t1
-
-    def get_vol(self, prices, span, delta):
-        df0 = prices.index.searchsorted(prices.index - delta)
-        df0 = df0[df0 > 0]
-        df0 = pd.Series(
-            prices.index[df0-1], index=prices.index[prices.shape[0]-df0.shape[0]:])
-        df0 = prices.loc[df0.index] / prices.loc[df0.values].values - 1
-        df0 = df0.ewm(span=span).std()
-        return df0
-
-    def get_touches(self, prices, events, factors=[1, 1]):
-        out = events[['t1']].copy(deep=True)
-        if factors[0] > 0:
-            thresh_uppr = factors[0] * events['threshold']
+def get_3_barriers(daily_volatility, t_final, upper_lower_multipliers, prices):
+    # create a container
+    barriers = pd.DataFrame(columns=['days_passed', 'price', 'vert_barrier',
+                            'top_barrier', 'bottom_barrier'], index=daily_volatility.index)
+    for day, vol in daily_volatility.iteritems():
+        days_passed = len(daily_volatility.loc[daily_volatility.index[0]: day])
+        # set the vertical barrier
+        if (days_passed + t_final < len(daily_volatility.index)
+                and t_final != 0):
+            vert_barrier = daily_volatility.index[
+                days_passed + t_final]
         else:
-            thresh_uppr = pd.Series(index=events.index)
-        if factors[1] > 0:
-            thresh_lwr = -factors[1] * events['threshold']
+            vert_barrier = np.nan
+        # set the top barrier
+        if upper_lower_multipliers[0] > 0:
+            top_barrier = prices.loc[day] + prices.loc[day] * \
+                upper_lower_multipliers[0] * vol
         else:
-            thresh_lwr = pd.Series(index=events.index)
-        for loc, t1 in events['t1'].iteritems():
-            df0 = prices[loc:t1]
-            df0 = (df0 / prices[loc] - 1) * events.side[loc]
-            out.loc[loc, 'stop_loss'] = df0[df0 < thresh_lwr[loc]].index.min()
-            out.loc[loc, 'take_profit'] = df0[df0 >
-                                              thresh_uppr[loc]].index.min()
-        return out
+            # set it to NaNs
+            top_barrier = pd.Series(index=prices.index)
+        # set the bottom barrier
+        if upper_lower_multipliers[1] > 0:
+            bottom_barrier = prices.loc[day] - prices.loc[day] * \
+                upper_lower_multipliers[1] * vol
+        else:
+            # set it to NaNs
+            bottom_barrier = pd.Series(index=prices.index)
+        barriers.loc[day, ['days_passed', 'price',
+                           'vert_barrier', 'top_barrier', 'bottom_barrier']] = \
+            days_passed, prices.loc[day], vert_barrier, \
+            top_barrier, bottom_barrier
+    barriers['out'] = None
+    return barriers
 
-    def get_labels(self, touches):
-        out = touches.copy(deep=True)
-        first_touch = touches[['stop_loss', 'take_profit']].min(axis=1)
-        for loc, t in first_touch.iteritems():
-            if pd.isnull(t):
-                out.loc[loc, 'label'] = 0
-            elif t == touches.loc[loc, 'stop_loss']:
-                out.loc[loc, 'label'] = -1
+
+def get_labels(barriers):
+    for i in range(len(barriers.index)):
+        start = barriers.index[i]
+        end = barriers.vert_barrier[i]
+        if pd.notna(end):
+            # assign the initial and final price
+            price_initial = barriers.price[start]
+            price_final = barriers.price[end]
+    # assign the top and bottom barriers
+            top_barrier = barriers.top_barrier[i]
+            bottom_barrier = barriers.bottom_barrier[i]
+    # set the profit taking and stop loss conditons
+            condition_pt = (barriers.price[start: end] >=
+                            top_barrier).any()
+            condition_sl = (barriers.price[start: end] <=
+                            bottom_barrier).any()
+    # assign the labels
+            if condition_pt:
+                barriers['out'][i] = 1
+            elif condition_sl:
+                barriers['out'][i] = -1
             else:
-                out.loc[loc, 'label'] = 1
-        return out
+                barriers['out'][i] = 0
+    return barriers
 
-    __cusum = cusum
-    __get_horizons = get_horizons
-    __get_vol = get_vol
-    __get_touches = get_touches
-    __get_labels = get_labels
 
-    def __init__(self, data, horizons_delta):
-        self.data = data
+def create_HLCV(i, data_stock):
+    df = pd.DataFrame(index=data_stock.index)
+    df[f'high_{i}'] = data_stock.high.rolling(i).max()
+    df[f'low_{i}'] = data_stock.low.rolling(i).min()
+    df[f'close_{i}'] = data_stock.close.rolling(i).\
+        apply(lambda x: x[-1])
+    df[f'volume_{i}'] = data_stock.volume.rolling(i).sum()
+    df[f'rsi_{i}'] = talib.RSI(data_stock['close']).rolling(i).mean()
 
-        self.__cusum(data['close'], 0.5, data)
+    return df
 
-        self.horizons = self.__get_horizons(data['close'], horizons_delta)
-        self.threshold = self.__get_vol(data['close'], 100, horizons_delta)
-        data_ohlc = pd.DataFrame(data=data)
-        data_ohlc = data_ohlc.assign(t1=self.horizons).dropna()
-        data_ohlc = data_ohlc.assign(threshold=self.threshold).dropna()
-        self.events = data_ohlc[['t1', 'threshold']]
-        self.events = self.events.assign(side=pd.Series(1., self.events.index))
 
-        self.touches = self.__get_touches(data['close'], self.events, [1, 1])
-        self.labels = self.__get_labels(self.touches)
+def create_features(i, data_stock):
+    df = create_HLCV(i, data_stock)
+    high = df[f'high_{i}']
+    low = df[f'low_{i}']
+    close = df[f'close_{i}']
+    volume = df[f'volume_{i}']
+    # rsi = df[f'rsi_{i}']
+    features = pd.DataFrame(index=data_stock.index)
+    features[f'volume_{i}'] = volume
+    features[f'price_spread_{i}'] = high - low
+    features[f'close_loc_{i}'] = (high - close) / (high - low)
+    features[f'close_change_{i}'] = close.diff()
+    # features[f'rsi_{i}'] = rsi
 
-        self.labels = self.labels.assign(
-            t1_value=data.loc[self.labels['t1']]['close'].values)
-        self.labels = self.labels.assign(stop_loss_value=[data.loc[sl]['close'] if not pd.isna(
-            sl) else np.nan for sl in self.labels['stop_loss'].values])
-        self.labels = self.labels.assign(take_profit_value=[data.loc[sl]['close'] if not pd.isna(
-            sl) else np.nan for sl in self.labels['take_profit'].values])
+    return features
 
-        plt.figure(figsize=(15, 5))
-        plt.plot(data.index, data['close'])
-        plt.plot(self.labels['stop_loss'].values,
-                 self.labels['stop_loss_value'].values, '.', label="stop_loss")
-        plt.plot(self.labels['take_profit'].values,
-                 self.labels['take_profit_value'].values, '.', label="take_profit_value")
 
-        plt.legend(loc='best')
-        plt.title('labels')
-        plt.grid(True)
+def create_bunch_of_features(data_stock):
+    days = [1, 2, 3, 5, 6, 7, 8, 9, 10, 20, 40, 60]
+    bunch_of_features = pd.DataFrame(index=data_stock.index)
+    for day in days:
+        f = create_features(day, data_stock)
+        bunch_of_features = bunch_of_features.join(f)
 
-        plt.figure(figsize=(15, 5))
-        plt.plot(data.index[-100:], data['close'][-100:], label="data")
-        plt.plot(self.labels['stop_loss'].values[-100:],
-                 self.labels['stop_loss_value'].values[-100:], '.', label="stop_loss")
-        plt.plot(self.labels['take_profit'].values[-100:],
-                 self.labels['take_profit_value'].values[-100:], '.', label="take_profit_value")
-        plt.title('labels last 100')
-        plt.legend(loc='best')
-        plt.grid(True)
+    # bunch_of_features['hour'] = data_stock.index.hour
+    return bunch_of_features
+
+
+def get_data_with_labels(symbol):
+    data_stock = get_data_by_hour(symbol)
+    price = data_stock['close']
+
+    df0 = get_Daily_Volatility(price)
+    daily_volatility = get_Daily_Volatility(price)
+    t_final = 10
+    upper_lower_multipliers = [3, 2]
+    prices = price[daily_volatility.index]
+    barriers = get_3_barriers(
+        daily_volatility, t_final, upper_lower_multipliers, prices)
+    barriers = get_labels(barriers)
+    bunch_of_features = create_bunch_of_features(data_stock)
+
+    return data_stock, barriers, bunch_of_features
